15b0d252d7d7009a541e0a2d66bc5f40155da3ca
safinhr <hrsafin2434@gmail.com>
2024-02-08 17:23:17 +0600

    problem 2 modified

--- problem2.js
+++ problem2.js
@@ -1,21 +1,19 @@
 //a, y, i , e , o , u, w
 
-let vowelList = ["a","y","i","e","u","w"]
-function checkName(name){
-   if(typeof name !== "string"){
-    return "Invalid"
-   } 
-   for(let vowel of vowelList){
-    if(name.includes(vowel)){
-        return "Good Name"
+function checkName(name) {
+  let vowelList = ["a", "y", "i", "e", "u", "w"];
+  if (typeof name !== "string") {
+    return "Invalid";
+  }
+  for (let vowel of vowelList) {
+    if (name.includes(vowel)) {
+      return "Good Name";
+    } else {
+      return "Bad Name";
     }
-    else{
-        return "Bad Name"
-    }
-   } 
-  
+  }
 }
 
-console.log(checkName("sfn"))
-console.log(checkName(12))
-console.log(checkName(["s"]))
+console.log(checkName("sfn"));
+console.log(checkName(12));
+console.log(checkName(["s"]));
