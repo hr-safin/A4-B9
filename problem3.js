91cc60ef752d8aa7ef00406f18ad0853461e61f7
safinhr <hrsafin2434@gmail.com>
2024-02-08 16:55:30 +0600

    problem 4 solved

--- problem3.js
+++ problem3.js
@@ -1,1 +1,17 @@
 //
+
+function deleteInvalids(array) {
+    if (!Array.isArray(array)) {
+      return "Invalid Array";
+    }
+  
+    let newArray = [];
+    for (let arr of array) {
+      if (typeof arr === "number" && !isNaN(arr)) {
+        newArray.push(arr); 
+      }
+    }
+    return newArray;
+  }
+
+console.log(deleteInvalids([1, 2, 4, NaN, undefined, {name : "ss"}])) ;
---
+++ problem4.js
@@ -0,0 +1,12 @@
+//
+
+function password(obj) {
+    //write your code here
+    let password = ""
+    for(let value in obj){
+         console.log(obj.siteName)
+    }
+}
+
+//{ name: “kolimuddin” , birthYear: 1999 , siteName: “google” }
+password({name : "kolimuddin", birthYear : 1999, siteName : "Google" })
